d2a504eceb925dc9561205469f86d0fb20c12394
Wesley Kaihara <wesleykaihara@gmail.com>
2023-08-09 21:21:38 -0300

    ajustes

--- trabalho/TDD-trabalho/tests/unitTests/example.test.js
+++
@@ -1,8 +0,0 @@
-const { expect } = require('chai');
-const { describe, it } = require('mocha');
-
-describe("#Example" , () => {
-  it("should be return a lower number" , () => {
-    expect(21).to.be.lessThan(20  )
-  })
-})
